521ef9345090ccde2be6e75c5ae8f5198ed1a7e4
Yuko Shionji <mrx0806@gmail.com>
2017-02-13 00:50:08 +0900

    コメントを追加

--- detect_cercle/uart/src/MB_uart_communication.cpp
+++ detect_cercle/uart/src/MB_uart_communication.cpp
@@ -14,24 +14,36 @@ int main(int argc,char **argv){
   ros::init(argc,argv,"MB_uart_communication");
   ros::NodeHandle n;
   ros::Publisher MBdata_pub = n.advertise<detect_cercle::MBinput>("MBdata", 5);
+  // MBから受け取ったデータをメイン処理プロセスに引き渡す.
   ros::Subscriber Jdata_sub = n.subscribe("Jdata",5,subscribe_Jdata);
-  
+  // 処理されたデータをMBに引き渡すためにこちらに渡す.
+
   uart_flag=try_connect_MB();
+  // uart接続が正しく出来ているかどうかを格納する.
 
   while(ros::ok()){
+    // メインループ
+
     if(false==uart_flag){
+      // uart接続が切れていたら接続し直す.
       ROS_INFO("MB is disconnected.\n");
       uart_flag=try_connect_MB();
     }
     ros::Rate loop_rate(main_loop_hz);
+    // ループ周期を規定
+
     int8_t MB_pole=0,color=0;
     float x=0,y=0,theta=0;
     bool success=false;
-    
+    // MBからデータを受け取るための変数.
+
     get_uart_input(&MB_pole,&color,&x,&y,&theta,&success);
+    // MBからデータを受け取る.
     publish_MBdata(MB_pole,color,x,y,theta,success,MBdata_pub);
+    // MBからのデータをメイン処理プロセスに引き渡す.
 
     ros::spinOnce();
+    // 処理されたデータを受け取るコールバック関数の呼び出しが可能なら行われる.
     //loop_rate.sleep();
     }
 }
@@ -41,19 +53,34 @@ bool try_connect_MB(void){
     シリアルポートをオープンできなければ周波数 connect_loop_hz で再試行し続ける.
    */
   int open_success;
+  // シリアルポートが開けたかどうかを格納する.
+
   open_success=open_serial_port(serial_dev);
+  // シリアルポートオープン
+
   ros::Rate loop_rate(connect_loop_hz);
+  // ループ周期を規定
+
   while(-1==open_success){
+    // シリアルポートが開けなかった時のループ.
+
     ROS_INFO("fail to open serial port.");
     ROS_INFO("try %dHz to open serial port.\n",connect_loop_hz);
     open_success=open_serial_port(serial_dev);
     loop_rate.sleep();
+    // 一定周期で再接続を試み続ける.
+
   }
   return true;
+  // ここに来れていたら必ずポートをオープンで来てる.
 }
 
 void get_uart_input(int8_t *MB_pole,int8_t *color,float *x,float *y,float *theta,bool *success){
+  //  MBからのデータを受け取る.
+
   if('X'==get_serial_char()){
+    // 最初の通信文字を検知したら処理を開始.
+
     const unsigned char n=get_serial_char();
     *MB_pole=(int8_t)n;
     const unsigned char c=get_serial_char();
@@ -68,6 +95,7 @@ void get_uart_input(int8_t *MB_pole,int8_t *color,float *x,float *y,float *theta
     const unsigned char th=get_serial_char();
     const int theta_i=(int)((int16_t)(tl|th<<8));
     *theta=(float)theta_i/100.0/180*M_PI;
+
     const unsigned char calc_sum=n+c+xl+xh+yl+yh+tl+th;
     const unsigned char calc_val=get_serial_char();
 
@@ -84,17 +112,17 @@ void get_uart_input(int8_t *MB_pole,int8_t *color,float *x,float *y,float *theta
 void publish_MBdata(int8_t MB_pole,int8_t color,float x,float y,float theta,bool success,ros::Publisher MBdata_pub){
   if(success){
     detect_cercle::MBinput MBinfo;
-    
+
     MBinfo.MB_pole=MB_pole;
     MBinfo.color=color;
     MBinfo.x=x;
     MBinfo.y=y;
     MBinfo.theta=theta;
     MBinfo.get_time=ros::Time::now();
-    
+
     MBdata_pub.publish(MBinfo);
     ROS_INFO("MBdata::MB_pole:%d,color:%d,x:%f,y:%f,theta:%f\n",(int)MB_pole,(int)color,x,y,theta);
-  } 
+  }
 }
 
 void subscribe_Jdata(const detect_cercle::Joutput& Jdata){
@@ -110,7 +138,7 @@ bool uart_output(int8_t MB_pole,float x,float y){
     char Y_H=(char)(send_y>>8);
 
     int i=0;
-    
+
     i+=put_serial_char('X');
     i+=put_serial_char(X_H);
     i+=put_serial_char(X_L);
